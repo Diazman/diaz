59eaf966eb28f36f6d930218f9bcca03083da218
Diazman <50883507+Diazman@users.noreply.github.com>
2023-10-20 18:45:51 +0300

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,14 +1,14 @@
 from telethon import TelegramClient, events
-from handlers import text_handler
+
 # Replace these values with your own API ID, API hash, and phone number
 api_id = 1150846
 api_hash = "d50ee5d1e6ba3b7950f3c38f08935f6f"
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-async def forward_messages(event):
-        destination = 541004638
-        await client.edit_message(destination, event.message, "Hello there" )
+@client.on(events.NewMessage(outgoing=True, pattern=".hi"))
+async def assalomu_alaykum(event):
+        await client.edit_message(event.message, "Assalomu alaykum" )
 
 client.start()
 client.run_until_disconnected()
